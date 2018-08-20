9c1387fedd21f01a722678ba75b8f9c5247d40d5
Jerry Wu <wuj828@gmail.com>
2018-08-19 22:33:40 -0700

    implemented isUnique function

--- ch01-arrays-strings/__tests__/isUnique.test.js
+++ ch01-arrays-strings/__tests__/isUnique.test.js
@@ -2,4 +2,12 @@ const isUnique = require('../isUnique');
 
 test('isUnique should be defined', () => {
   expect(isUnique).toBeDefined;
+
+test('isUnique should return true for a string of unique characters', () => {
+  expect(isUnique('dog')).to.equal(true);
+
+test('isUnique should return false for a string containing repeating characters', () => {
+  expect(isUnique('doggo')).to.equal(false);
+
+})
 });
--- ch01-arrays-strings/isUnique.js
+++ ch01-arrays-strings/isUnique.js
@@ -1,4 +1,19 @@
-const isUnique = () => {
+const isUnique = (string) => {
+  let countCharacters = {}
+
+  //Build up countCharacters object to keep count of unique keys
+  string.split('').forEach(character => {
+    if (!countCharacters.hasOwnProperty(character)) {
+      countCharacters[character] = 1;
+    } else {
+      countCharacters[character] += 1;
+    }
+  })
+
+  //Filters array of countCharacter's values to see if greater than 1. If so, not all characters are unique (return false).
+  if (Object.values(countCharacters).filter(frequency => frequency > 1).length !== 0) {
+    return false;
+  }
   return true;
 };
 
