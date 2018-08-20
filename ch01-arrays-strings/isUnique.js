627e975698f8d2f6631d5d44791b55471f7a9697
Jerry Wu <wuj828@gmail.com>
2018-08-19 22:37:07 -0700

    Comments/white-space revised.

--- ch01-arrays-strings/isUnique.js
+++ ch01-arrays-strings/isUnique.js
@@ -1,6 +1,7 @@
 const isUnique = (string) => {
   //Declare empty object which will be used to store string frequency data
   let countCharacters = {}
+
   //Create keys for each string (if they do not exist in object) and set frequency to 1. Add 1 if key exists already.
   string.split('').forEach(character => {
     if (!countCharacters.hasOwnProperty(character)) {
@@ -9,6 +10,7 @@ const isUnique = (string) => {
       countCharacters[character] += 1;
     }
   })
+  
   //Filters array of countCharacter's values to see if greater than 1. If so, not all characters are unique (return false).
   if (Object.values(countCharacters).filter(frequency => frequency > 1).length !== 0) {
     return false;
